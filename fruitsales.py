04ab2f7f95b61674bb16aaf991ede05d0bf13d8c
Unurjargal Boldbaatar <aja20.aja10success@gmail.com>
2023-08-24 20:05:42 -0400

    Adding code

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,11 @@
-# add your code here
+
+
+import pandas  as pd
+
+friuts = pd.DataFrame(
+    {"Apples": [35, 41],
+    "Bananas": [21,  34]},
+index = ["2017 Sales" , "2018 Sales"])
+
+print(fruits)
+
